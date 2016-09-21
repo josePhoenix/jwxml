8b110ac2a8cc3ef83b24788491313d62ec63b895
Joseph Long <jlong@stsci.edu>
2016-09-21 15:52:55 -0400

    List OS support (all) in setup.py

--- setup.py
+++ setup.py
@@ -54,6 +54,9 @@ setup(
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: BSD License',
 
+        # OS support
+        'Operating System :: OS Independent',
+
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
