f4b5c8d7e51da161a63cb84aef5917a35200202b
Joseph Long <jlong@stsci.edu>
2016-09-20 22:06:37 -0400

    Finish setup.py

--- setup.py
+++ setup.py
@@ -24,23 +24,20 @@ with open(path.join(root, NAME, 'VERSION'), encoding='utf-8') as f:
 setup(
     name=NAME,
 
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
+    # Versions should comply with PEP440.
     version=version,
 
     description='Parse and manipulate XML files, mostly JWST optics and wavefront sensing related.',
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/pypa/sampleproject',
+    url='https://github.com/mperrin/jwxml',
 
     # Author details
-    author='The Python Packaging Authority',
-    author_email='pypa-dev@googlegroups.com',
-
-    # Choose your license
-    license='MIT',
+    author='Marshall Perrin, Joseph Long',
+    author_email='help@stsci.edu',
+    license='BSD',
+    keywords='jwst xml siaf',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -52,10 +49,10 @@ setup(
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
+        'Topic :: Scientific/Engineering :: Astronomy',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: BSD License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
@@ -65,26 +62,23 @@ setup(
         'Programming Language :: Python :: 3.5',
     ],
 
-    # What does your project relate to?
-    keywords='jwst xml siaf',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=['jwxml'],
+    packages=find_packages(),
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['peppercorn'],
+    install_requires=['numpy>=1.9', 'matplotlib>=1.4.3'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
     extras_require={
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
+        'dev': ['check-manifest>=0.34', 'lxml>=3.6.4'],
     },
 
     # If there are data files included in your packages that need to be
@@ -93,21 +87,7 @@ setup(
     package_data={
         'jwxml': [
             'VERSION',
-        ],
-    },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[('my_data', ['data/data_file'])],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    entry_points={
-        'console_scripts': [
-            'sample=sample:main',
+            'data/*/*.xml'
         ],
     },
 )
