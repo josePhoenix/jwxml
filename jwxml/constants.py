96e515e26a087b2db8f4b8c5a18b7d8c6127090c
Marshall Perrin <mperrin@stsci.edu>
2017-03-31 17:17:58 -0400

    Update to draft April 2017 SIAF files.

--- jwxml/constants.py
+++ jwxml/constants.py
@@ -3,5 +3,5 @@ import os.path
 __all__ = ['PRD_VERSION', 'DATA_ROOT', 'PRD_DATA_ROOT']
 
 DATA_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
-PRD_VERSION = 'PRDOPSSOC-E-002'  # updated 2016-11-11
+PRD_VERSION = 'DraftApril2017'  # updated 2017-03-31
 PRD_DATA_ROOT = os.path.join(DATA_ROOT, PRD_VERSION)
