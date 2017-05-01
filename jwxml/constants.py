290060139687ac87e2225fe646d880325a4ac921
Marshall Perrin <mperrin@stsci.edu>
2017-05-01 19:24:40 -0400

    April 2017 SIAF delivery, PRDOPSSOC-F-008. Closes #11

--- jwxml/constants.py
+++ jwxml/constants.py
@@ -3,5 +3,5 @@ import os.path
 __all__ = ['PRD_VERSION', 'DATA_ROOT', 'PRD_DATA_ROOT']
 
 DATA_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
-PRD_VERSION = 'DraftApril2017'  # updated 2017-03-31
+PRD_VERSION = 'PRDOPSSOC-F-008'  # updated 2017-04-26
 PRD_DATA_ROOT = os.path.join(DATA_ROOT, PRD_VERSION)
